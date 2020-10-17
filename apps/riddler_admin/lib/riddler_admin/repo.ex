5c600fcdf65ca9931446d10b71e8fce748fb80d8
JohnnyT <johnnyt@divvypay.com>
2020-10-17 10:21:47 -0600

    Updates Ecto config for primary keys

--- apps/riddler_admin/lib/riddler_admin/repo.ex
+++ apps/riddler_admin/lib/riddler_admin/repo.ex
@@ -2,4 +2,13 @@ defmodule RiddlerAdmin.Repo do
   use Ecto.Repo,
     otp_app: :riddler_admin,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    config =
+      config
+      |> Keyword.put_new(:migration_primary_key, name: :id, type: :text)
+      |> Keyword.put_new(:migration_foreign_key, column: :id, type: :text)
+
+    {:ok, config}
+  end
 end
