c11a74082991bbdf40a4fd14f46752367c5c80b3
JohnnyT <ubergeek3141@gmail.com>
2020-10-28 12:53:43 -0600

    Logs at info level in dev and prod

--- config/dev.exs
+++ config/dev.exs
@@ -47,6 +47,7 @@ config :riddler_admin_web, RiddlerAdminWeb.Endpoint,
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+config :logger, level: :info
 
 config :phoenix,
   # Initialize plugs at runtime for faster development compilation
