1a9bb2aab08ee8669f043f5ae5f029ffec99a4bf
JohnnyT <ubergeek3141@gmail.com>
2020-12-06 07:33:49 -0700

    Adds UI for Elements

--- apps/riddler_admin_web/lib/riddler_admin_web/router.ex
+++ apps/riddler_admin_web/lib/riddler_admin_web/router.ex
@@ -57,7 +57,11 @@ defmodule RiddlerAdminWeb.Router do
 
     resources "/workspaces", WorkspaceController do
       resources "/conditions", ConditionController
-      resources "/content_blocks", ContentBlockController
+
+      resources "/content_blocks", ContentBlockController do
+        resources "/elements", ElementController
+      end
+
       resources "/definitions", DefinitionController
 
       resources "/environments", EnvironmentController do
---
+++ apps/riddler_admin_web/lib/riddler_admin_web/views/element_view.ex
@@ -0,0 +1,3 @@
+defmodule RiddlerAdminWeb.ElementView do
+  use RiddlerAdminWeb, :view
+end
