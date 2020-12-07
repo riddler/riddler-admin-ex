bd8b2807aad4e900ce0d8f21243c09fe1575be7d
JohnnyT <ubergeek3141@gmail.com>
2020-12-07 07:11:45 -0700

    Adds rendering of text in ContentBlocks

--- apps/riddler_agent/lib/riddler_agent/guide/content_block_generator.ex
+++ apps/riddler_agent/lib/riddler_agent/guide/content_block_generator.ex
@@ -4,28 +4,38 @@ defmodule RiddlerAgent.Guide.ContentBlockGenerator do
   map of dynamic content.
   """
 
+  alias Liquid.Template
+
   alias RiddlerAgent.Guide.ContentBlock
 
   require Logger
 
-  def process(%{elements: elements} = content_block, context) when is_map(content_block) and not is_struct(content_block) do
+  def process(%{elements: elements} = content_block, context)
+      when is_map(content_block) and not is_struct(content_block) do
     element_structs =
       elements
       |> Enum.map(fn map -> struct(ContentBlock.Element, map) end)
 
-    converted_map =
-      %{content_block | elements: element_structs}
+    converted_map = %{content_block | elements: element_structs}
 
     process(struct(ContentBlock, converted_map), context)
   end
 
-  def process( %ContentBlock{elements: elements} = content_block, _context) do
-    generated_elements = 
+  def process(%ContentBlock{elements: elements} = content_block, context) do
+    generated_elements =
       elements
-    |> Enum.map(fn element ->
-      %{key: element.key, text: element.text}
-    end)
+      |> Enum.map(&generate_element(&1, context))
 
     %{key: content_block.key, elements: generated_elements}
+  end
+
+  defp generate_element(element, context) do
+    %{key: element.key, text: render_text(element.text, context)}
+  end
+
+  defp render_text(text, context) do
+    template = Template.parse(text)
+    {:ok, rendered, _context} = Template.render(template, context)
+    rendered
   end
 end
