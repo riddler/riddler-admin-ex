9493e0d07245b0aebefe859e0a490e70d3a83022
JohnnyT <ubergeek3141@gmail.com>
2020-12-11 07:18:56 -0700

    Adds Variant elements to the Guide

--- apps/riddler_agent/lib/riddler_agent/guide/content_block.ex
+++ apps/riddler_agent/lib/riddler_agent/guide/content_block.ex
@@ -2,6 +2,6 @@ defmodule RiddlerAgent.Guide.ContentBlock do
   defstruct [:key, elements: []]
 
   defmodule __MODULE__.Element do
-    defstruct [ :type, :key, :text ]
+    defstruct [:type, :key, :text, :include_instructions, :elements]
   end
 end
--- apps/riddler_agent/test/riddler_agent/guide/content_block_generator/variant_element_test.exs
+++ apps/riddler_agent/test/riddler_agent/guide/content_block_generator/variant_element_test.exs
@@ -17,9 +17,9 @@ defmodule RiddlerAgent.Guide.ContentBlockGenerator.VariantElementTest do
           elements: [
             %{
               id: "el_WELCOME_USR",
-              include_instructions: [["load", "user_id"], ["to_bool"]],
+              include_instructions: [["load", "user_id"], ["lit", 1], ["compare", "EQ"]],
               include_source: "user_id",
-              text: "Hi User {{ user_id }}.",
+              text: "Hi User {{ user_id }}",
               type: "Text"
             },
             %{
@@ -39,8 +39,27 @@ defmodule RiddlerAgent.Guide.ContentBlockGenerator.VariantElementTest do
     test "with no context uses the last element", %{content_block: content_block} do
       result = Generator.process(content_block, %{})
 
-      assert %{key: "welcome_message", elements: [%{key: "welcome_message", text: "Welcome"}]} =
-               result
+      assert %{
+               key: "welcome_message",
+               elements: [
+                 %{key: "welcome_message", text: "Welcome visitor"}
+               ]
+             } = result
+
+      element = result.elements |> hd
+
+      refute Map.has_key?(element, :elements)
+    end
+
+    test "with correct context uses the first element", %{content_block: content_block} do
+      result = Generator.process(content_block, %{"user_id" => 1})
+
+      assert %{
+               key: "welcome_message",
+               elements: [
+                 %{key: "welcome_message", text: "Hi User 1"}
+               ]
+             } = result
 
       element = result.elements |> hd
 
